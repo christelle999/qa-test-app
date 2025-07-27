da827a2dd377947363aefe3d18321a4cec0ce375
christelle youssef <67819779+christelle999@users.noreply.github.com>
2025-07-27 18:59:30 +0300

    Create Homepage.js

---
+++ frontend/src/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div>
+      <h2>Welcome to Homepage</h2>
+      <p>This is the main page after logging in.</p>
+    </div>
+  );
+}
+
+export default Homepage;
